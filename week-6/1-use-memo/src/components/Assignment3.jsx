3bcc695720016c9d44616dfa46dd5a17a7142861
Deepak <deepakadluri@gmail.com>
2024-06-28 16:55:24 +0530

    assignments

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -12,7 +12,13 @@ export const Assignment3 = () => {
     ]);
 
     // Your code starts here
-    const totalValue = 0;
+
+    const totalValue = useMemo(() => {
+        const totalValue = 0;
+        totalValue = items.reduce((a, b) => a + b)
+        return totalValue
+    }, [items])
+
     // Your code ends here
     return (
         <div>
